403907ae731db17e97e7db476d48b586572217fe
Sandeep Kumar Jakkaraju <sandeepkumar.jakkaraju@outlook.com>
2021-03-21 03:03:28 +0530

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,14 +10,11 @@ username: admin
 password: admin
 
 */
-
 INSERT INTO admin(ID,admin_id,admin_pwd) VALUES (1,'admin', 'admin')
-
 
 /*
  inserting product categories
 */
-
 INSERT INTO category (ID,name) VALUES (1,'Category1')
 INSERT INTO category (ID,name) VALUES (2,'Category2')
 INSERT INTO category (ID,name) VALUES (3,'Category3')
@@ -27,7 +24,6 @@ INSERT INTO category (ID,name) VALUES (5,'Category5')
 /*
 inserting products
 */
-
 INSERT INTO eproduct (ID, category_id, date_added, name, price) VALUES (1, 1, now(), 'Product1', 1000)
 INSERT INTO eproduct (ID, category_id, date_added, name, price) VALUES (2, 2, now(), 'Product2', 1001)
 INSERT INTO eproduct (ID, category_id, date_added, name, price) VALUES (3, 3, now(), 'Product3', 1003)
